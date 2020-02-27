795cc6d453457cc7924478e5cd3a7235f0517309
Shooter2832 <v3y2b@ugrad.cs.ubc.ca>
2020-02-26 19:48:32 -0800

    Revised Code Coverage

--- src/main/persistence/ReaderTest.java
+++ src/main/persistence/ReaderTest.java
@@ -17,14 +17,14 @@ class ReaderTest {
         try {
             List<Playlist> playlists = Reader.readPlaylists(new File("./data/testPlaylist.txt"));
             Playlist p1 = playlists.get(0);
-            assertEquals("JordanW",p1.getTag());
+            assertEquals("Jordan",p1.getTag());
             assertEquals("Hood Beats", p1.getPlaylistName());
             assertEquals("Rap", p1.getPlaylistGenre());
             assertEquals(2, p1.getTotalSongs());
             assertEquals(500, p1.getTotalRuntime());
 
             Playlist p2 = playlists.get(1);
-            assertEquals("JordanW",p2.getTag());
+            assertEquals("Jordan",p2.getTag());
             assertEquals("Country Thunder", p2.getPlaylistName());
             assertEquals("Country", p2.getPlaylistGenre());
             assertEquals(1, p2.getTotalSongs());
